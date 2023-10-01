e127a77a564066a11a043c2fd8a359263cc4d2ce
Franco May <franco.may@etraveligroup.com>
2023-10-01 17:41:30 +0200

    made the player into a sphere

--- src/prefabs/Player.tsx
+++ src/prefabs/Player.tsx
@@ -6,12 +6,9 @@ import {
   GizmoViewport,
 } from "@react-three/drei";
 import { useRef } from "react";
-import { PublicApi, useCompoundBody } from "@react-three/cannon";
+import { PublicApi, useSphere } from "@react-three/cannon";
 import { Controls } from "../models";
-import {
-  Vector3,
-  Mesh,
-} from "three";
+import { Vector3, Mesh } from "three";
 import { useEffect, MutableRefObject, useState } from "react";
 
 const useInput = () => {
@@ -22,15 +19,19 @@ const useInput = () => {
   const left = useKeyboardControls<Controls>((state) => state.left) ? 1 : 0;
   const right = useKeyboardControls<Controls>((state) => state.right) ? 1 : 0;
   const jump = useKeyboardControls<Controls>((state) => state.jump);
-  const [vector, setVector] = useState(new Vector3());
+  const [relativeMovementAngle, setRelativeMovementAngle] = useState(0);
   const [hasInput, setHasInput] = useState(false);
 
   useEffect(() => {
-    setVector(new Vector3((right - left), 0, (back - forward)));
+    const z = left - right;
+    const x = back - forward;
+    const angle = Math.atan2(x, z);
+    setRelativeMovementAngle(angle);
 
     if (forward || back || left || right) {
       setHasInput(true);
     } else {
+      setRelativeMovementAngle(0);
       setHasInput(false);
     }
   }, [forward, back, left, right]);
@@ -42,56 +43,17 @@ const useInput = () => {
     right,
     jump,
     hasInput,
-    vector
+    relativeMovementAngle,
   };
-}
+};
 
-const Player = () => {
-  const dampingFactor = 0.999999;
-  const [grounded, setGrounded] = useState(0);
-  const position = useRef(new Vector3());
-  const velocity = useRef(new Vector3());
-  const { camera } = useThree();
-  const { hasInput, vector, jump } = useInput();
-  const [isJumping, setIsJumping] = useState(false);
-
-  const [collissionMesh, playerPhysicsApi] = useCompoundBody(() => ({
-    mass: 80,
-    shapes: [
-      { args: [0.3], position: [0, 1.5, 0], type: "Sphere" },
-      { args: [0.3], position: [0, 0.9, 0], type: "Sphere" },
-      { args: [0.3], position: [0, 0.3, 0], type: "Sphere" },
-    ],
-    onCollide: (e) => {
-      if (e.contact.impactVelocity > 50) {
-        console.log("player died. or received damage.");
-      }
-    },
-    onCollideBegin: () => (setGrounded((grounded) => grounded + 1)),
-    onCollideEnd: () => (setGrounded((grounded) => grounded - 1)),
-    fixedRotation: true,
-    position: [0, 1, 0],
-    material: {
-      friction: 0
-    },
-  })) as [MutableRefObject<Mesh>, PublicApi];
-
-  useEffect(() => {
-  
-    if (jump && grounded > 0) {
-      playerPhysicsApi.velocity.set(velocity.current.x, velocity.current.y+10, velocity.current.z);
-    }
-  }, [jump, playerPhysicsApi]);
-
-  useEffect(() => {
-    if (hasInput || !grounded){
-      playerPhysicsApi.linearDamping.set(0);
-      return
-    }
-    playerPhysicsApi.linearDamping.set(dampingFactor);
-  }
-  , [hasInput, grounded, playerPhysicsApi]);
-
+const useAttachCamera = (
+  playerPhysicsApi: PublicApi | undefined,
+  camera: THREE.Camera | undefined,
+  collissionMesh: MutableRefObject<Mesh> | undefined,
+  position: MutableRefObject<Vector3>,
+  velocity: MutableRefObject<Vector3>
+) => {
   useEffect(() => {
     if (!playerPhysicsApi || !camera || !collissionMesh) return;
 
@@ -111,32 +73,88 @@ const Player = () => {
       velocity.current = new Vector3(x, y, z);
     });
   }, [playerPhysicsApi, camera]);
+};
+
+const useJump = (
+  jump: boolean,
+  grounded: number,
+  playerPhysicsApi: PublicApi | undefined,
+  velocity: MutableRefObject<Vector3>
+) => {
+  useEffect(() => {
+    if (!playerPhysicsApi) return;
+    if (jump && grounded > 0) {
+      playerPhysicsApi.velocity.set(
+        velocity.current.x,
+        velocity.current.y + 10,
+        velocity.current.z
+      );
+    }
+  }, [jump, playerPhysicsApi]);
+};
+
+const Player = () => {
+  const sphereRadius = 0.5;
+  const [grounded, setGrounded] = useState(0);
+  const position = useRef(new Vector3());
+  const velocity = useRef(new Vector3());
+  const { camera } = useThree();
+  const { hasInput, relativeMovementAngle, jump } = useInput();
+  const speed = 1000;
+
+  const [collissionMesh, playerPhysicsApi] = useSphere(() => ({
+    args: [sphereRadius],
+    mass: 80,
+    onCollide: (e) => {
+      if (e.contact.impactVelocity > 50) {
+        console.log("player died. or received damage.");
+      }
+    },
+    onCollideBegin: () => setGrounded((grounded) => grounded + 1),
+    onCollideEnd: () => setGrounded((grounded) => grounded - 1),
+    // fixedRotation: true,
+    position: [0, 1, 0],
+    material: {
+      friction: 0.5,
+    },
+  })) as [MutableRefObject<Mesh>, PublicApi];
+
+  useJump(jump, grounded, playerPhysicsApi, velocity);
+
+  useAttachCamera(playerPhysicsApi, camera, collissionMesh, position, velocity);
 
   useFrame((_props, delta) => {
     if (!playerPhysicsApi || !camera) return;
 
-    
-    // check if there is input and the player is grounded, else let physics handle movement
-    if (hasInput && grounded > 0) {
-      playerPhysicsApi.velocity.set(0, 0, 0);
+    if (!hasInput) {
+      // fix rotation
+      playerPhysicsApi.angularVelocity.set(0, 0, 0);
+    }
+
+    if (hasInput) {
       const angleYcameraDirection = camera.rotation.y;
+      const worldMovementAngle = relativeMovementAngle + angleYcameraDirection;
+      const deltaSpeed = speed * delta;
+
+      const x = Math.sin(worldMovementAngle) * deltaSpeed;
+      const z = Math.cos(worldMovementAngle) * deltaSpeed;
+
       playerPhysicsApi.rotation.set(0, angleYcameraDirection, 0);
-
-      const speed = 40000 * delta;
-      const direction = vector.clone().setLength(speed);
-
-      playerPhysicsApi.applyLocalImpulse(direction.toArray(), [0, 0, 0]);
+      playerPhysicsApi.angularVelocity.set(x, 0, z);
     }
   });
 
   return (
     <group name="player">
       <GizmoHelper>
-        <GizmoViewport axisColors={["red", "green", "blue"]} labelColor="black" />
+        <GizmoViewport
+          axisColors={["red", "green", "blue"]}
+          labelColor="black"
+        />
       </GizmoHelper>
       <PointerLockControls />
       <mesh ref={collissionMesh} castShadow receiveShadow name="player">
-        <capsuleGeometry args={[0.3, 1.3, 2, 10]} />
+        <sphereGeometry args={[sphereRadius, 32, 32]} />
         <meshStandardMaterial color="blue" />
       </mesh>
     </group>
