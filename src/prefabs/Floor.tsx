c4a5fabfe0c51e3064e4a7070771dacd5565036d
Franco May <franco.may@etraveligroup.com>
2023-10-02 20:03:21 +0200

    added texture to the floor

--- src/prefabs/Floor.tsx
+++ src/prefabs/Floor.tsx
@@ -1,4 +1,5 @@
 import { usePlane, PlaneProps } from "@react-three/cannon";
+import { useNormalTexture } from "@react-three/drei";
 
 const Floor = ({ material = "Ground", ...rest }: PlaneProps) => {
   const [ref] = usePlane(() => ({
@@ -6,10 +7,21 @@ const Floor = ({ material = "Ground", ...rest }: PlaneProps) => {
     material,
     ...rest,
   })) as any;
+
+  const [normalMap, _url] = useNormalTexture(17, {
+    offset: [0, 0],
+    repeat: [100, 100],
+  });
+
   return (
     <mesh ref={ref} receiveShadow name="floor">
       <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial color="lightblue" metalness={0.1} roughness={0.5} />
+      <meshStandardMaterial
+        color="lightblue"
+        metalness={0.1}
+        roughness={0.5}
+        normalMap={normalMap}
+      />
     </mesh>
   );
 };
--- src/scenes/Playground.tsx
+++ src/scenes/Playground.tsx
@@ -1,5 +1,5 @@
 import { Physics } from "@react-three/cannon";
-import { Environment } from "@react-three/drei";
+import { Environment, Sky } from "@react-three/drei";
 import Player from "../prefabs/Player";
 import Cube from "../prefabs/Cube";
 import Floor from "../prefabs/Floor";
@@ -7,7 +7,9 @@ import Floor from "../prefabs/Floor";
 const Scene = () => {
   return (
     <Physics>
-      <Environment preset="city" background />
+      <Environment background={false} preset="forest" />
+      <Sky inclination={1} azimuth={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
       <Player position={[0, 2, 0]} />
       <Floor />
       <Cube
